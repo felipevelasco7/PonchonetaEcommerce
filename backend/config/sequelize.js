d2f22da3fbcfd1f1a714e56f21ab5aa25cb6ca08
Felipe Velasco <100039794+felipevelasco7@users.noreply.github.com>
2025-05-20 19:54:13 -0500

    ajustes

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('ecommerce', 'root', '', {
-  host: 'ecommerce-db.c0pjtanm6hnq.us-east-1.rds.amazonaws.com',
+  host: 'ponchoneta-db.cmbn2jnrhzmq.us-east-1.rds.amazonaws.com',
   dialect: 'mysql'
 });
 
